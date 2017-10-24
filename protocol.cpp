ac0d0b2f0f969b774b87f59e74b27e2b39b1ed6c
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2017-10-24 03:48:13 +0300

    Changed PCH

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xe1, 0x47, 0x6d, 0x44 };
 
 CMessageHeader::CMessageHeader()
 {
